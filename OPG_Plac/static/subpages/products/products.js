6d61a0d19e49df6a2f49a6b8e00e1d5905cdb026
Marko <marko.pavic667@gmail.com>
2020-11-27 20:32:32 +0100

    Progress on products page framework and responsive design -- framework near complete

--- OPG_Plac/static/subpages/products/products.js
+++ OPG_Plac/static/subpages/products/products.js
@@ -1,6 +1,28 @@
 
-function open_close_category_submenu(id){
-    const category_option_element = document.querySelector(`#filter-option-id-${id}`)
+function open_close_category_submenu(id){  // Flips submenu_options classnames - shows or hides the menu
 
-    console.log(category_option_element)
+    const classNames = {
+        "submenu_active": "product-subfilter-container show_submenu",
+        "submenu_inactive": "product-subfilter-container",
+        "mark_menu_open": " submenu-open"
+    };
+
+    const target_block_element = document.querySelector(`#category-option-${id}`);
+    const target_submenu_element = document.querySelector(`#submenu-option-${id}`);
+
+    if(target_submenu_element.className === classNames["submenu_inactive"]){
+        target_submenu_element.className = classNames["submenu_active"];
+        target_block_element.className = target_block_element.className + classNames["mark_menu_open"];
+
+    } else{
+        target_submenu_element.className = classNames["submenu_inactive"];
+        target_block_element.className = target_block_element.className.replace(classNames["mark_menu_open"], "");
+    }
+
+    document.activeElement.getBoundingClientRect()
+}
+
+function mark_active(){
+    const classNames = {
+    }
 }
