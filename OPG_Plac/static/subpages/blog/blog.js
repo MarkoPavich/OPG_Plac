2f572a4fd1b70855d6aee549f107303d0a8eba32
Marko <marko.pavic667@gmail.com>
2020-12-27 11:24:33 +0100

    fix: blog_pagination net/previous handling

--- OPG_Plac/static/subpages/blog/blog.js
+++ OPG_Plac/static/subpages/blog/blog.js
@@ -45,8 +45,9 @@ function do_pagination(total_pages, current_page, active_filter){  // Handles 'n
 
     // Handle previous - next buttons
 
-    if(current_page === 1 || total_pages === 0) option_previous.className = "hide";
+    if(current_page <= 1) option_previous.className = "hide";
     else option_previous.firstElementChild.setAttribute("href", `${href_slug}${parseInt(current_page) - 1}`);
-    if(current_page === total_pages || total_pages === 0) option_next.className = "hide";
+
+    if(current_page >= total_pages || total_pages === 0) option_next.className = "hide";
     else option_next.firstElementChild.setAttribute("href", `${href_slug}${parseInt(current_page) + 1}`);
 }
